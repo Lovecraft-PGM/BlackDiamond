bc081be0f0abb79d3ab7162dfed175a2a1fe4221
Jonathan.MB <Marinjonathan40@gmail.com>
2023-04-13 20:10:27 -0500

    descarga de jquery al proyecto

--- resources/views/cabeceraSimple.blade.php
+++ resources/views/cabeceraSimple.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="icon" type="image/png" href="{{ asset('img/iconoBD.ico') }}">
+        <script src="js/jquery-3.6.4.min.js"></script>
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         
         <title>Black Diamond</title>
